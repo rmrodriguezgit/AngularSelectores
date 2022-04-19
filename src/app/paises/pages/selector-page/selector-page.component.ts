b97e7434baa79582155509f0e218bd7f25d1a8c0
Ruben Rodriguez <mirodrig@hotmail.com>
2022-04-18 23:51:38 -0500

    Primer Commit

---
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
+
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent implements OnInit {
+
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', Validators.required ]
+  }); 
+
+  //Llenar selectores
+  regiones: string[] = [];
+
+
+  constructor(private fb: FormBuilder,
+              private paisesService: PaisesService ) { }
+
+  ngOnInit(): void {
+
+    this.regiones = this.paisesService.regiones;
+
+
+  }
+
+  guardar() {
+    console.log(this.miFormulario.value);
+  }
+
+}
---
+++ src/app/paises/services/paises.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisesService {
+
+  private _regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  get regiones(): string[] {
+    return [ ...this._regiones ];
+  }
+  constructor() { }
+}
